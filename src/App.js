c8e8a885bfcabffc57a6ce2f1d90ad096b87a810
vishnu k <vishnukkakkarayil@gmail.com>
2023-03-30 11:02:08 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
+  const day = new Date().toLocaleDateString(undefined,{weekday:'long'})
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {day} 🌝 ☕</h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(event) => setToDo(event.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => {
+          setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+          setToDo('');
+          }
+        } className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((value) => {
+            return (
+              <div key={value.id} className="todo">
+                <div className="left">
+                  <input onChange={(event) => {
+                    console.log(event.target.checked)
+                    console.log(value)
+                    setToDos(toDos.filter(obj => {
+                      if (obj.id === value.id) {
+                        obj.status = event.target.checked
+                      }
+                      return obj
+                    }))
+                  }} value={value.status} type="checkbox" name="" id="" />
+                  <p>{value.text}</p>
+                </div>
+                <div className="right">
+                  <i onClick={() => {setToDos(toDos.filter((todo) => todo.id !== value.id))}} className="fas fa-times"></i>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
 
 export default App;
+
